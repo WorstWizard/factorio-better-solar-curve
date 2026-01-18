61fca5377c20ed8ec383a4e5e1132a6a0462cb5d
WorstWizard <worstwizard@pm.me>
2026-01-18 21:54:06 +0100

    fix!: use storage instead of global for 2.0

--- control.lua
+++ control.lua
@@ -2,35 +2,32 @@
 --- Corresponding math constants: E, n, d, D
 --- https://www.desmos.com/calculator/peke1jt4pp
 local function compute_constants()
-    global.const = {}
-    global.const.E = game.surfaces["nauvis"].dusk
-    global.const.n = game.surfaces["nauvis"].evening
-    global.const.d = game.surfaces["nauvis"].morning
-    global.const.D = game.surfaces["nauvis"].dawn
-    global.const.pre_slope = -1.0/(global.const.n - global.const.E)
-    global.const.post_slope = 1.0/(global.const.D - global.const.d)
-    global.const.pre_mul = math.pi/(2.0*global.const.n)
-    global.const.post_mul = math.pi/(2.0*(1.0 - global.const.d))
-    local power_before = (2.0+global.const.E + global.const.n - global.const.d - global.const.D)/2.0
-    local power_after = 1.0/global.const.pre_mul + 1.0/global.const.post_mul
-    global.const.correction_mul = power_before/power_after
+    storage.const = {}
+    storage.const.E = game.surfaces["nauvis"].dusk
+    storage.const.n = game.surfaces["nauvis"].evening
+    storage.const.d = game.surfaces["nauvis"].morning
+    storage.const.D = game.surfaces["nauvis"].dawn
+    storage.const.pre_slope = -1.0/(storage.const.n - storage.const.E)
+    storage.const.post_slope = 1.0/(storage.const.D - storage.const.d)
+    storage.const.pre_mul = math.pi/(2.0*storage.const.n)
+    storage.const.post_mul = math.pi/(2.0*(1.0 - storage.const.d))
+    local power_before = (2.0+storage.const.E + storage.const.n - storage.const.d - storage.const.D)/2.0
+    local power_after = 1.0/storage.const.pre_mul + 1.0/storage.const.post_mul
+    storage.const.correction_mul = power_before/power_after
 end
 
 local function pre_curve(time)
-    return math.cos(time*global.const.pre_mul)
+    return math.cos(time*storage.const.pre_mul)
 end
 local function post_curve(time)
-    return math.cos((1.0-time)*global.const.post_mul)
+    return math.cos((1.0-time)*storage.const.post_mul)
 end
 
-script.on_init(function ()
-        compute_constants()
-    end
-)
+script.on_init(compute_constants)
 script.on_event(defines.events.on_tick,
     function (event)
         local surface = game.surfaces["nauvis"]
-        local c = global.const
+        local c = storage.const
         local t = surface.daytime
         local modifier
         if t > c.D then
